8fbe2e431644990ebfba01f84efe90b239f8349c
stanah <foo.exe@gmail.com>
2023-10-17 00:11:11 +0900

    remove unused

--- app/scripts/content.tsx
+++ app/scripts/content.tsx
@@ -1,14 +1,9 @@
 import ReactDOM from "react-dom/client";
 
-import { getAnimeData } from "./App/danimeClient";
 import App from "./App";
 
-getAnimeData("26440").then((anime) => {
-  console.log(anime);
-  const div = document.createElement("div");
-  document.body.insertBefore(div, document.body.firstChild);
+const div = document.createElement("div");
+document.body.insertBefore(div, document.body.firstChild);
 
-  // document.body.appendChild(div);
-  const root = ReactDOM.createRoot(div);
-  root.render(<App />);
-});
+const root = ReactDOM.createRoot(div);
+root.render(<App />);
