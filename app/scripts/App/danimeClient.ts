c7b19b9c2f1cef924081734d4a3bb79206520fc4
stanah <foo.exe@gmail.com>
2023-10-18 22:19:13 +0900

    update danimeClient

--- app/scripts/App/danimeClient.ts
+++ app/scripts/App/danimeClient.ts
@@ -1,14 +1,20 @@
 import axios from "axios";
 
+const BASE_URL = "https://animestore.docomo.ne.jp/animestore/ci_pc?workId=";
+
+export type AnimeId = number;
+// export type AnimeId = string;
+
 export interface Anime {
-  id: number;
+  id: AnimeId;
   title: string;
   description: string;
   seasonTag: string;
-  updated_at: Date;
+  updated_at: number; //timestamp
   episodes: Episode[];
   allWatched: boolean;
   unWatchedCount: number;
+  latestUnwatchedEpisodeUrl: string;
 }
 
 export interface Episode {
@@ -17,7 +23,7 @@ export interface Episode {
   title: string;
   url: string;
   watched: boolean;
-  created_at: Date;
+  created_at: number;
 }
 
 // 文字列から数値を抽出する
@@ -27,15 +33,9 @@ const extractNumberFromString = (str: string): number => {
   return Number(number[0]);
 };
 
-const getAnimeData = async (id: string): Promise<Anime> => {
+const getAnimeData = async (id: AnimeId): Promise<Anime> => {
   const address = `https://animestore.docomo.ne.jp/animestore/ci_pc?workId=${id}`;
-  // const response = await axios.get(address);
-  const response = await axios.get(address, {
-    // withCredentials: true,
-    // headers: {
-    //   Cookie: cookie,
-    // },
-  });
+  const response = await axios.get(address);
   const dom = new DOMParser().parseFromString(response.data, "text/html").body;
   const episodeList: Episode[] = [];
 
@@ -44,16 +44,13 @@ const getAnimeData = async (id: string): Promise<Anime> => {
     if (match == null || match.length === 0) return;
     const imgSrc = element.querySelector("div.thumbnailContainer img")?.getAttribute("data-src") || "";
     const createdAt = imgSrc.slice(-13);
-
     episodeList.push({
       id: element.id,
       episodeNumber: extractNumberFromString(element.querySelector("div.textContainer span.line1 span.number")?.innerHTML ?? ""),
       title: element.querySelector("div.textContainer h3.line2 span")?.innerHTML ?? "",
-      url: `https://animestore.docomo.ne.jp/animestore/ci_pc?workId=${id}&${element
-        .getAttribute("href")
-        ?.replace("http://localhost:4200/cd_pc?", "")}`, // FIXME
+      url: `https://animestore.docomo.ne.jp/animestore/ci_pc?workId=${id}&${element.getAttribute("href")?.replace("cd_pc?", "")}`, // FIXME
       watched: element.classList.contains("watched"),
-      created_at: new Date(Number(createdAt)),
+      created_at: new Date(Number(createdAt)).getTime(),
     });
   });
   // spanタグを削除
@@ -78,11 +75,31 @@ const getAnimeData = async (id: string): Promise<Anime> => {
     episodes: episodeList,
     allWatched: episodeList.every((episode) => episode.watched),
     unWatchedCount: episodeList.filter((episode) => !episode.watched).length,
+    latestUnwatchedEpisodeUrl: episodeList.find((episode) => !episode.watched)?.url ?? `${BASE_URL}${id}`,
   };
 };
 
-const login = async () => {
-  throw new Error("Not implemented");
+export interface AnimeSimple {
+  title: string;
+  workId: string;
+}
+
+// https://animestore.docomo.ne.jp/animestore/mp_viw_pc
+const getList = async () => {
+  const address = `https://animestore.docomo.ne.jp/animestore/mp_viw_pc`;
+  const response = await axios.get(address);
+  const dom = new DOMParser().parseFromString(response.data, "text/html").body;
+  const watchList: AnimeSimple[] = [];
+  dom.querySelectorAll("div.itemModule a.textContainer").forEach((element) => {
+    const href = element.getAttribute("href");
+    const title = element.querySelector("h2.line1 span")?.innerHTML || "";
+    // URLをパースして、workIdとpartIdを取得する
+    const workId = href?.match(/workId=(\d+)/)?.[1] || "";
+    // const partId = href?.match(/partId=(\d+)/)?.[1] || "";
+    watchList.push({ title, workId });
+  });
+
+  return watchList;
 };
 
-export { getAnimeData, login };
+export { getAnimeData, getList };
